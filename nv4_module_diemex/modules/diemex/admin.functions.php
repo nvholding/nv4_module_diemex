64a220853fe84e750a4d82064fbec3a3095fdb9f
Nguyễn Thanh Hoàng <hoang.nguyen@webvang.vn>
2021-11-23 16:46:57 +0700

    Update and rename admin.functions to admin.functions.php

--- nv4_module_diemex/modules/diemex/admin.functions.php
+++ nv4_module_diemex/modules/diemex/admin.functions.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @Project NUKEVIET 3.4
+ * @Project NUKEVIET 4.5
  * @Author hongoctrien (01692777913@yahoo.com)
  * @Update to 4.x webvang (hoang.nguyen@webvang.vn)
  * @Copyright (C) 2012 by hongoctrien
@@ -11,7 +11,10 @@
 if( ! defined( 'NV_ADMIN' ) or ! defined( 'NV_MAINFILE' ) or ! defined( 'NV_IS_MODADMIN' ) ) die( 'Stop!!!' );
 
 define( 'NV_IS_FILE_ADMIN', true );
-
+  $allow_func = [
+          'main',
+          'notice'
+    ];
 
 function nv_diemex_get_bodytext( $bodytext )
 {
